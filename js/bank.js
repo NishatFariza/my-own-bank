193d31e17b2f74c0badc0658725421a49a4fdac9
NishatFariza <nishatfarzia78@gmail.com>
2022-02-11 01:07:36 +0600

    blance and transaction added

---
+++ js/bank.js
@@ -0,0 +1,17 @@
+document.getElementById('login-submit').addEventListener('click', function(){
+    //get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail =emailField.value ;
+
+    //get password
+    const passwordlField = document.getElementById('user-password');
+    const userPassword = passwordlField.value ;
+
+    //check user email and password
+    if(userEmail == 'abc@gmail.com' && userPassword == 'secret'){
+        console.log('this is right');
+        window.location.href ='banking.html';
+
+    }
+
+})
